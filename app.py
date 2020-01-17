72692028bc13e8f5fa49d6201f83f4cbec4cb03d
phadkem <51807611+phadkem@users.noreply.github.com>
2020-01-17 17:43:00 -0600

    add /placeBets route and inital email input on page

--- app.py
+++ app.py
@@ -9,4 +9,8 @@ def welcome():
     #temporary page to test herokuapp connection
     return render_template('welcome.html')
 
+@app.route('/', methods=["POST","GET"]) 
+def placeBets():
+    return render_template('placeBets.html')
+
 if __name__ == '__main__': app.run(debug=True)
